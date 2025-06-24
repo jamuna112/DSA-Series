c6c89531fd6252ddd51931085aa0bcf80711f417
Jamuna Adhikari <jamunaadhikari112@gmail.com>
2025-06-24 14:32:49 -0700

    solve reverse string problem

---
+++ string/reverse-string.go
@@ -0,0 +1,20 @@
+package string
+
+/*
+LEETCODE
+344. Reverse String
+*/
+
+func ReverseString(str []byte) []byte {
+
+	startPtr := 0
+	endPtr := len(str) - 1
+
+	for startPtr < endPtr {
+		str[startPtr], str[endPtr] = str[endPtr], str[startPtr]
+		startPtr++
+		endPtr--
+	}
+
+	return str
+}
---
+++ string/reverse-string_test.go
@@ -0,0 +1,30 @@
+package string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseString(t *testing.T) {
+
+	tests := []struct {
+		input    []byte
+		expected []byte
+	}{
+		{[]byte{'h', 'e', 'l', 'l', 'o'}, []byte{'o', 'l', 'l', 'e', 'h'}},
+		{[]byte{}, []byte{}},
+		{[]byte{'o', 'r', 'a', 'n', 'g', 'e'}, []byte{'e', 'g', 'n', 'a', 'r', 'o'}},
+	}
+
+	for _, tt := range tests {
+		//copy the input slice
+		inputCopy := make([]byte, len(tt.input))
+		copy(inputCopy, tt.input)
+		output := ReverseString(inputCopy)
+
+		if !reflect.DeepEqual(output, tt.expected) {
+			t.Errorf("Failed, expecting %v but got %v", tt.expected, output)
+		}
+
+	}
+}
