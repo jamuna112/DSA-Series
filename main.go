86fbbdedb3f5d7c5d7550acd1f6436299393ca0c
jamuna112 <jamunaadhikari112@gmail.com>
2025-07-14 09:15:17 -0700

    Doubly linked list push front function

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "dsa-series/lnklist"
+
 func main() {
 
+	list := &lnklist.DoublyList{}
+
+	list.Push_Front(5)
+	list.Push_Front(6)
+	list.Push_Front(7)
+	list.PrintDoublyList()
 }
