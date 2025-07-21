6664d40123f9958854f25d67362083d0d05beb39
jamuna112 <jamunaadhikari112@gmail.com>
2025-07-21 08:27:42 -0700

    next largest element 1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"dsa-series/lnklist"
+	"dsa-series/stack"
 	"fmt"
 )
 
@@ -12,19 +13,25 @@ func main() {
 	list.Push_Front(5)
 	list.Push_Front(6)
 	list.Push_Front(7)
-	fmt.Println("===== Push front function =====")
-	list.PrintDoublyList()
+	// fmt.Println("===== Push front function =====")
+	// list.PrintDoublyList()
 
 	list.Push_Back(1)
-	fmt.Println("\n===== Push back function =====")
-	list.PrintDoublyList()
+	// fmt.Println("\n===== Push back function =====")
+	// list.PrintDoublyList()
 
-	fmt.Println("\n===== Pop Front function =====")
-	list.Pop_Front()
-	list.PrintDoublyList()
+	// fmt.Println("\n===== Pop Front function =====")
+	// list.Pop_Front()
+	// list.PrintDoublyList()
 
-	fmt.Println("\n===== Pop Back function =====")
-	list.Pop_Back()
-	list.PrintDoublyList()
+	// fmt.Println("\n===== Pop Back function =====")
+	// list.Pop_Back()
+	// list.PrintDoublyList()
+
+	nums1 := []int{4, 1, 2}
+	nums2 := []int{1, 3, 4, 2}
+
+	ans := stack.NextGreaterElement1(nums1, nums2)
+	fmt.Print("ans is ", ans)
 
 }
